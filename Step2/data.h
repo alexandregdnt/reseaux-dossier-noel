97578bb515bc59cb28685471cd7ab9f668319e07
Alexandre Grodent <alexandregrodent@gmail.com>
2021-11-22 16:50:42 +0100

    Update data.h Step 2 for the Transmission

--- Step2/data.h
+++ Step2/data.h
@@ -2,19 +2,20 @@
 #define DATAH
 
 struct VehiculeAG {
-    int  Reference ;
-    char Marque[30] ;
-    char Modele[30] ;
-    int  Quantite ;
-    char Date[10] ;
+    int  Reference;
+    char Marque[30];
+    char Modele[30];
+    char Transmission[10];
+    int  Quantite;
+    char Date[10];
 };
 
 struct FactureAG{
-    int NumeroFacturation ;
-    char NomClient[40] ;
-    int DateFacturation ;
-    int Quantite ;
-    int Reference ;
+    int NumeroFacturation;
+    char NomClient[40];
+    int DateFacturation;
+    int Quantite;
+    int Reference;
 };
 
 #endif
