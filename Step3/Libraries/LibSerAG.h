aa73b7a7a198a8acbadd50d2d0b3cd4fb05d5ef0
Alexandre Grodent <alexandregrodent@gmail.com>
2021-11-29 17:07:28 +0100

    :100: Step 3: Finish

---
+++ Step3/Libraries/LibSerAG.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "data.h"
+
+void AProposServeurAG (char *, char *, char*);
+
+int RechercheAG(char *, int, struct VehiculeAG *);
---
+++ Step3/Libraries/data.h
@@ -0,0 +1,21 @@
+#ifndef DATAH
+#define DATAH
+
+struct VehiculeAG {
+    int  Reference;
+    char Marque[30];
+    char Modele[30];
+    char Transmission[10];
+    int  Quantite;
+    char Date[10];
+};
+
+struct FactureAG{
+    int NumeroFacturation;
+    char NomClient[40];
+    int DateFacturation;
+    int Quantite;
+    int Reference;
+};
+
+#endif
